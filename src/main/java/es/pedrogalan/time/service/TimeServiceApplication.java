eb052d9563db14aa7cafd6d4518e6a955fcecddf
Pedro Galan <pedro.galan@piksel.com>
2020-06-09 19:28:36 +0100

    Register the service in Eureka

--- src/main/java/es/pedrogalan/time/service/TimeServiceApplication.java
+++ src/main/java/es/pedrogalan/time/service/TimeServiceApplication.java
@@ -2,8 +2,10 @@ package es.pedrogalan.time.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TimeServiceApplication {
 
 	public static void main(String[] args) {
