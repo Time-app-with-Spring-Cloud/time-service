50293f4bd0b26405b3d8ea89e90a5e7d6da82393
Pedro Galan <pedro.galan@piksel.com>
2020-06-11 08:54:05 +0100

    Add servide unique id to the response to play with load balancing

--- src/main/java/es/pedrogalan/time/service/controller/TimeController.java
+++ src/main/java/es/pedrogalan/time/service/controller/TimeController.java
@@ -2,6 +2,7 @@ package es.pedrogalan.time.service.controller;
 
 import es.pedrogalan.time.service.TimeServiceConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +16,13 @@ public class TimeController {
     @Autowired
     private TimeServiceConfiguration configuration;
 
+    @Value("${eureka.instance.instance-id}")
+    private String instanceId;
+
     @RequestMapping("/")
     public String time() {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern(configuration.getTimeformat());
         LocalDateTime now = LocalDateTime.now();
-        return dtf.format(now);
+        return dtf.format(now) + "(from " + instanceId + ")";
     }
 }
